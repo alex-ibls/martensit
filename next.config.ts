93eac0daa0bdcda2ccded388d420d2ce01527a3f
Bocharov Alexander <aleksandr.bocharov@mvideo.ru>
2026-08-24 18:57:20 +0300

    cmt

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/acme-challenge/:file",
+        headers: [
+          { key: "Content-Type", value: "text/plain" },
+          { key: "Cache-Control", value: "no-store" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
